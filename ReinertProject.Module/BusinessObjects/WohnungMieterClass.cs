e68ebadd5fd89558066f243707af68d3688a8a36
Guillermo-Prieto <guiprirod@alum.us.es>
2023-12-19 18:41:43 +0100

    Add project files.

---
+++ ReinertProject.Module/BusinessObjects/WohnungMieterClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace ReinertProject.Module.BusinessObjects.Database
+{
+
+    public partial class WohnungMieterClass
+    {
+        public WohnungMieterClass(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
