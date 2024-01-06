4546e05277ec0a682ccdc5c45871e4e7b09214d2
Guillermo-Prieto <guiprirod@alum.us.es>
2024-01-06 23:00:52 +0100

    Add new button header

--- ReinertProject.Blazor.Server/BlazorApplication.cs
+++ ReinertProject.Blazor.Server/BlazorApplication.cs
@@ -4,42 +4,60 @@ using DevExpress.ExpressApp.Blazor;
 using DevExpress.ExpressApp.Security;
 using DevExpress.ExpressApp.Security.ClientServer;
 using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
 using DevExpress.ExpressApp.Xpo;
 using ReinertProject.Blazor.Server.Services;
+using ReinertProject.Blazor.Server.Templates;
 
 namespace ReinertProject.Blazor.Server;
 
-public class ReinertProjectBlazorApplication : BlazorApplication {
-    public ReinertProjectBlazorApplication() {
+public class ReinertProjectBlazorApplication : BlazorApplication
+{
+    public ReinertProjectBlazorApplication()
+    {
         ApplicationName = "ReinertProject";
         CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
         DatabaseVersionMismatch += ReinertProjectBlazorApplication_DatabaseVersionMismatch;
     }
-    protected override void OnSetupStarted() {
+    protected override IFrameTemplate CreateDefaultTemplate(TemplateContext context)
+    {
+        if (context == TemplateContext.ApplicationWindow)
+        {
+            return new CustomApplicationWindowTemplate();
+        }
+        return base.CreateDefaultTemplate(context);
+    }
+    protected override void OnSetupStarted()
+    {
         base.OnSetupStarted();
 #if DEBUG
-        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+        if (System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema)
+        {
             DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
         }
 #endif
     }
-    private void ReinertProjectBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+    private void ReinertProjectBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e)
+    {
 #if EASYTEST
         e.Updater.Update();
         e.Handled = true;
 #else
-        if(System.Diagnostics.Debugger.IsAttached) {
+        if (System.Diagnostics.Debugger.IsAttached)
+        {
             e.Updater.Update();
             e.Handled = true;
         }
-        else {
+        else
+        {
             string message = "The application cannot connect to the specified database, " +
                 "because the database doesn't exist, its version is older " +
                 "than that of the application or its schema does not match " +
                 "the ORM data model structure. To avoid this error, use one " +
                 "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
 
-            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+            if (e.CompatibilityError != null && e.CompatibilityError.Exception != null)
+            {
                 message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
             }
             throw new InvalidOperationException(message);
---
+++ ReinertProject.Module/Controllers/CustomWindowController.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReinertProject.Module.Controllers
+{
+    public class CustomWindowController : WindowController
+    {
+
+        public SimpleAction MyCustomAction { get; private set; }
+
+        public CustomWindowController()
+        {
+
+            MyCustomAction = new SimpleAction(this, nameof(MyCustomAction), DevExpress.Persistent.Base.PredefinedCategory.Menu)
+            {
+                //Specify the Action's button caption.
+                Caption = "Action",
+                //Specify the confirmation message that pops up when a user executes an Action.
+                ConfirmationMessage = "This is a simple action",
+                //Specify the icon of the Action's button in the interface.
+                ImageName = "Action_Clear"
+            };
+
+
+            MyCustomAction.Execute += MyCustomAction_Execute;
+        }
+
+        private void MyCustomAction_Execute(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
